2b90906fad8994e99c362e24778c452b82a21ead
spbeck <stephenpbeck@gmail.com>
2020-04-26 08:06:18 -0700

    Remove menu labels that don't do anything. Reorganize menu creation into one function iterating over a nested dictionary.

--- text-editor.py
+++ text-editor.py
@@ -25,7 +25,9 @@ def exit_command():
         root.destroy()
 
 def about_command():
-    label = messagebox.showinfo("About","Simple Text Editor by Stephen Beck")
+    label = messagebox.showinfo("About",
+                                "Simple Text Editor by Stephen Beck made in "
+                                "Python3 and Tkinter.")
 
 def work():
     print("Working, please wait...")
@@ -41,53 +43,28 @@ def norm():
 menu_1 = Menu(root)
 root.config(menu=menu_1)
 
-# First Menu
-file_menu = Menu(menu_1)
-menu_1.add_cascade(label="File",
-                   menu=file_menu)
+# Define menus in a dictionary so we can iterate over them to create 'em.
+menus = {"File":{"New file":work,
+                 "Open":open_command,
+                 "Save":save_command,
+                 "Exit":exit_command},
+         "Edit":{"Resize":res,
+                 "Normal":norm},
+         "View":{"About":about_command}}
 
-file_menu.add_command(label="New file",
-                      command=work)
-file_menu.add_command(label="Open",
-                      command=open_command)
-file_menu.add_command(label="Save",
-                      command=save_command)
-file_menu.add_command(label="Exit",
-                      command=exit_command)
+# Add menus
+for m in menus:
+    current_menu = Menu(menu_1)
+    menu_1.add_cascade(label=m,
+                       menu=current_menu)
+    for n in menus[m]:
+       current_menu.add_command(label=n,
+                                command=menus[m][n])
 
-# Second Menu
-edit_menu=Menu(menu_1)
-menu_1.add_cascade(label="Edit",
-                   menu=edit_menu)
-edit_menu.add_command(label="Resize",
-                      command=res)
-edit_menu.add_command(label="Normal",
-                      command=norm)
-edit_menu.add_cascade(label="Cut")
-edit_menu.add_cascade(label="Copy")
-edit_menu.add_cascade(label="Paste")
 
-# Third Menu
-view_menu = Menu(menu_1)
-menu_1.add_cascade(label="View",
-                   menu=view_menu)
-view_menu.add_command(label="About",
-                      command=about_command)
-
-# Fourth Menu
-code_menu = Menu(menu_1)
-menu_1.add_cascade(label="Code",
-                   menu=code_menu)
-
-# Fifth Menu
-run_menu = Menu(menu_1)
-menu_1.add_cascade(label="Run",
-                   menu=run_menu)
-
-# Sixth Menu
-help_menu = Menu(menu_1)
-menu_1.add_cascade(label="Help",
-                   menu=help_menu)
+# Missing features for the future here:
+    # -- Recognize & run code that's entered into the text editor
+    # -- Cut, copy & paste features
 
 # Add status bar
 status = Label(root,
@@ -98,8 +75,3 @@ status.pack(side=BOTTOM,fill=X)
 
 textPad.pack()
 root.mainloop()
-
-
-
-
-
